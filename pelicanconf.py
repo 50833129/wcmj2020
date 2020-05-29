62248291de492af402e0e1932e6394c85cbf612d
50833129 <50833129@gm.nfu.edu.tw>
2020-05-29 13:10:52 +0800

    w12

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '50833129'
+SITENAME = '2020 wcmj 網際日誌'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
